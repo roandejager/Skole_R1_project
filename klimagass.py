45fd7ca523e3b10f4ffb70a6c90989699242d59d
Owner <roan.de.jager08@gmail.com>
2026-05-12 10:09:17 +0200

    updated klimagass.py

--- klimagass.py
+++ klimagass.py
@@ -11,16 +11,20 @@ veitrafikk = klima.iloc[:, 5]
 motorfart = klima.iloc[:, 6]
 
 lowess = sm.nonparametric.lowess
-z1 = lowess(olje, aar, frac = 0.2, return_sorted = False)
-z2 = lowess(industri, aar, frac = 0.2, return_sorted = False)
-z5 = lowess(veitrafikk, aar, frac = 0.2, return_sorted = False)
-z6 = lowess(motorfart, aar, frac = 0.2, return_sorted = False)
+z1 = lowess(olje, aar, frac=0.2, return_sorted=False)
+z2 = lowess(industri, aar, frac=0.2, return_sorted=False)
+z5 = lowess(veitrafikk, aar, frac=0.2, return_sorted=False)
+z6 = lowess(motorfart, aar, frac=0.2, return_sorted=False)
 
-plt.plot(aar, olje)
-plt.plot(aar, z1, "r")
-plt.plot(aar, z2, "b")
-plt.plot(aar, z5, "w")
-plt.plot(aar, z6, "p")
+plt.plot(aar, olje, label="Olje (Rådata)", alpha=0.3)
+plt.plot(aar, z1, "r", label="Olje (Trend)")
+plt.plot(aar, z2, "b", label="Industri")
+plt.plot(aar, z5, "g", label="Veitrafikk")
+plt.plot(aar, z6, "m", label="Motorfart")
+
 plt.xlabel("År")
 plt.ylabel("Temperatur (grader C)")
+
+plt.legend(loc="upper left")
+
 plt.show()
