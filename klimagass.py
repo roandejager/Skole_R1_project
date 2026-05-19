3710e3ea025f8436030a98e18a9809c83f8fa5d3
doglerABC <minhk1356@gmail.com>
2026-05-19 09:03:01 +0200

    Update klimagass.py

--- klimagass.py
+++ klimagass.py
@@ -1,6 +1,7 @@
+import math
+import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
-import matplotlib.pyplot as plt
 
 klima = pd.read_csv("figur-1-totalt-klimagass.csv", sep=";", decimal=",")
 
@@ -13,18 +14,38 @@ motorfart = klima.iloc[:, 6]
 lowess = sm.nonparametric.lowess
 z1 = lowess(olje, aar, frac=0.2, return_sorted=False)
 z2 = lowess(industri, aar, frac=0.2, return_sorted=False)
-z5 = lowess(veitrafikk, aar, frac=0.2, return_sorted=False)
-z6 = lowess(motorfart, aar, frac=0.2, return_sorted=False)
+z3 = lowess(veitrafikk, aar, frac=0.2, return_sorted=False)
+z4 = lowess(motorfart, aar, frac=0.2, return_sorted=False)
 
-plt.plot(aar, olje, label="Olje (Rådata)", alpha=0.3)
+n = len(aar)
+o_snitt = sum(olje) / n
+i_snitt = sum(industri) / n
+v_snitt = sum(veitrafikk) / n
+m_snitt = sum(motorfart) / n
+
+o_s = math.sqrt(sum((olje - o_snitt) ** 2) / (n - 1))
+i_s = math.sqrt(sum((industri - i_snitt) ** 2) / (n - 1))
+v_s = math.sqrt(sum((veitrafikk - v_snitt) ** 2) / (n - 1))
+m_s = math.sqrt(sum((motorfart - m_snitt) ** 2) / (n - 1))
+
+o_se = o_s / math.sqrt(n)
+i_se = i_s / math.sqrt(n)
+v_se = v_s / math.sqrt(n)
+m_se = m_s / math.sqrt(n)
+
+print(f'Gjennomsnittet for oljens CO2-utslipp gjennom årene er {round(o_snitt, 4)}, standardavvik {round(o_s, 4)} og standardfeil {round(o_se, 4)}')
+print(f'Gjennomsnittet for industriens CO2-utslipp gjennom årene er {round(i_snitt, 4)}, standardavvik {round(i_s, 4)} og standardfeil {round(i_se, 4)}')
+print(f'Gjennomsnittet for veitrafikkens CO2-utslipp gjennom årene er {round(v_snitt, 4)}, standardavvik {round(v_s, 4)} og standardfeil {round(v_se, 4)}')
+print(f'Gjennomsnittet for motortrafikkens CO2-utslipp gjennom årene er {round(m_snitt, 4)}, standardavvik {round(m_s, 4)} og standardfeil {round(m_se, 4)}')
+
 plt.plot(aar, z1, "r", label="Olje (Trend)")
 plt.plot(aar, z2, "b", label="Industri")
-plt.plot(aar, z5, "g", label="Veitrafikk")
-plt.plot(aar, z6, "m", label="Motorfart")
+plt.plot(aar, z3, "g", label="Veitrafikk")
+plt.plot(aar, z4, "m", label="Motorfart")
 
 plt.xlabel("År")
-plt.ylabel("Temperatur (grader C)")
+plt.ylabel("Million tonn CO2")
 
-plt.legend(loc="upper left")
+plt.legend(loc="upper right")
 
 plt.show()
